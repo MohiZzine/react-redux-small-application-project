06973f0823ab007e282bf92f01e5ba1f04106b22
Mohieddine Farid <farid.mohieddine3@gmail.com>
2023-04-04 13:45:55 +0000

    Use of redux in react for increment/decrement action

---
+++ src/components/Counter.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { useSelector, useDispatch, connect } from "react-redux";
+
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
